60b58141b824f495380d6f2b303a30dce8e10083
qizhihui628 <q8471@163.com>
2015-01-13 21:54:57 +0800

    Update Air2.

--- NEW/2/DAIKIN/crebas.sql
+++ NEW/2/DAIKIN/crebas.sql
@@ -1,40 +1,20 @@
-drop table Power_Day_Record;
+drop table Air_Module1;
 
-drop table Power_RT_Record;
+drop table Air_Module_Register;
 
-drop table Power_Status;
-
-create table Power_Day_Record (
+create table Air_Module1 (
 ID                   INTEGER                        not null,
-Energy_L1            FLOAT,
-Energy_L2            FLOAT,
-Energy_L3            FLOAT,
-TimeStamp            TIMESTAMP,
+Air_ID               INTEGER,
+Status               VARCHAR(64),
 primary key (ID)
 );
 
-create table Power_RT_Record (
-ID                   INTEGER                        not null,
-Voltage_L1           FLOAT,
-Current_L1           FLOAT,
-Power_L1             FLOAT,
-Energy_L1            FLOAT,
-Voltage_L2           FLOAT,
-Current_L2           FLOAT,
-Power_L2             FLOAT,
-Energy_L2            FLOAT,
-Voltage_L3           FLOAT,
-Current_L3           FLOAT,
-Power_L3             FLOAT,
-Energy_L3            FLOAT,
-TimeStamp            TIMESTAMP,
-primary key (ID)
-);
-
-create table Power_Status (
+create table Air_Module_Register (
 ID                   INTEGER                        not null,
 Address              INTEGER,
-Running_State        LONG VARBINARY,
+Table_Name           VARCHAR(64),
+Module_Type          VARCHAR(64),
+Status               VARCHAR(64),
 primary key (ID)
 );
 
